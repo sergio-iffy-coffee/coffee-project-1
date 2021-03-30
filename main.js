f965e80a721ba6fb5393ef94cf15231aa70f91af
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-03-30 11:44:59 -0500

    feat: worked on coffee

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 "use strict"
 
 function renderCoffee(coffee) {
-    var html = '<tr class="coffee">';
-    html += '<td>' + coffee.id + '</td>';
-    html += '<td>' + coffee.name + '</td>';
-    html += '<td>' + coffee.roast + '</td>';
-    html += '</tr>';
+    var html = '<div class="coffee">';
+    html += '<h1>' + coffee.name + '</h1>';
+    html += '<p>' + coffee.roast + '</p>';
+    html += '</div>';
 
     return html;
 }
@@ -47,6 +46,9 @@ var coffees = [
     {id: 13, name: 'Italian', roast: 'dark'},
     {id: 14, name: 'French', roast: 'dark'},
 ];
+
+coffees.reverse();
+
 
 var tbody = document.querySelector('#coffees');
 var submitButton = document.querySelector('#submit');
